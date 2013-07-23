e457d0c53f62416efd149cdc862370739ca10b2e
MikeOntry <christophersisco@gmail.com>
2013-07-23 08:33:38 -0500

    added shout functions
    Shout in friends, party and MU feed.
    Delete shout in friends, party, and MU feed.
    Retrieve shouts from friends, party, and MU feed.

--- src/Erpk/Harvester/Module/Management/ManagementModule.php
+++ src/Erpk/Harvester/Module/Management/ManagementModule.php
@@ -211,4 +211,162 @@ class ManagementModule extends Module
         $response = $request->send()->json();
         return $response;
     }
+    
+    public function shoutFriend($message)
+    {
+        $this->getClient()->checkLogin();
+        $request = $this->getClient()->post('main/wall-post/create/');
+        $request->getHeaders()
+            ->set('X-Requested-With', 'XMLHttpRequest')
+            ->set('Referer', $this->getClient()->getBaseUrl());
+    	$request->addPostFields(
+            array(
+				'post_message' => $message,
+                '_token'  => $this->getSession()->getToken()
+            )
+		);
+        $response = $request->send()->json();
+        return $response;
+    }
+	
+	public function shoutParty($message)
+    {
+        $this->getClient()->checkLogin();
+        $request = $this->getClient()->post('main/party-post/create/');
+        $request->getHeaders()
+            ->set('X-Requested-With', 'XMLHttpRequest')
+            ->set('Referer', $this->getClient()->getBaseUrl());
+		$request->addPostFields(
+            array(
+				'post_message' => $message,
+                '_token'  => $this->getSession()->getToken()
+            )
+		);
+        $response = $request->send()->json();
+        return $response;
+    }
+	
+	public function shoutMU($message, $groupId, $post_as=1)
+    {
+        $this->getClient()->checkLogin();
+        $request = $this->getClient()->post('main/group-wall/create/post');
+        $request->getHeaders()
+            ->set('X-Requested-With', 'XMLHttpRequest')
+            ->set('Referer', $this->getClient()->getBaseUrl());
+		$request->addPostFields(
+            array(
+				'post_message' => $message,
+				'groupId' => $groupId,
+				'post_as' => $post_as,
+                '_token'  => $this->getSession()->getToken()
+            )
+		);
+        $response = $request->send()->json();
+        return $response;
+    }
+	
+	public function delShoutFriend($postId)
+    {
+        $this->getClient()->checkLogin();
+        $request = $this->getClient()->post('main/wall-post/delete/');
+        $request->getHeaders()
+            ->set('X-Requested-With', 'XMLHttpRequest')
+            ->set('Referer', $this->getClient()->getBaseUrl());
+		$request->addPostFields(
+            array(
+				'postId' => $postId,
+                '_token'  => $this->getSession()->getToken()
+            )
+		);
+        $response = $request->send()->json();
+        return $response;
+    }
+	
+	public function delShoutParty($postId)
+    {
+        $this->getClient()->checkLogin();
+        $request = $this->getClient()->post('main/party-post/delete/');
+        $request->getHeaders()
+            ->set('X-Requested-With', 'XMLHttpRequest')
+            ->set('Referer', $this->getClient()->getBaseUrl());
+		$request->addPostFields(
+            array(
+				'postId' => $postId,
+                '_token'  => $this->getSession()->getToken()
+            )
+		);
+        $response = $request->send()->json();
+        return $response;
+    }
+	
+	public function delShoutMU($postId, $groupId)
+    {
+        $this->getClient()->checkLogin();
+        $request = $this->getClient()->post('main/group-wall/create/post');
+        $request->getHeaders()
+            ->set('X-Requested-With', 'XMLHttpRequest')
+            ->set('Referer', $this->getClient()->getBaseUrl());
+		$request->addPostFields(
+            array(
+				'postId' => $postId,
+				'groupId' => $groupId,
+                '_token'  => $this->getSession()->getToken()
+            )
+		);
+        $response = $request->send()->json();
+        return $response;
+    }
+	
+	public function getFriendFeed($page)
+    {
+        $this->getClient()->checkLogin();
+        $request = $this->getClient()->post('main/wall-post/older/');
+        $request->getHeaders()
+            ->set('X-Requested-With', 'XMLHttpRequest')
+            ->set('Referer', $this->getClient()->getBaseUrl());
+		$request->addPostFields(
+            array(
+				'page' => $page,
+                '_token'  => $this->getSession()->getToken()
+            )
+		);
+        $response = $request->send();
+        return $response->getBody(true);
+    }
+	
+	public function getPartyFeed($page)
+    {
+        $this->getClient()->checkLogin();
+        $request = $this->getClient()->post('main/party-post/older/');
+        $request->getHeaders()
+            ->set('X-Requested-With', 'XMLHttpRequest')
+            ->set('Referer', $this->getClient()->getBaseUrl());
+		$request->addPostFields(
+            array(
+				'page' => $page,
+                '_token'  => $this->getSession()->getToken()
+            )
+		);
+        $response = $request->send();
+        return $response->getBody(true);
+    }
+	
+	public function getMUFeed($page, $groupId)
+    {
+        $this->getClient()->checkLogin();
+        $request = $this->getClient()->post('main/group-wall/older/retrieve');
+        $request->getHeaders()
+            ->set('X-Requested-With', 'XMLHttpRequest')
+            ->set('Referer', $this->getClient()->getBaseUrl());
+		$request->addPostFields(
+            array(
+				'groupId' => $groupId,
+				'page' => $page,
+				'part' => 1,
+                '_token'  => $this->getSession()->getToken()
+            )
+		);
+        $response = $request->send();
+        return $response->getBody(true);
+    }
 }
